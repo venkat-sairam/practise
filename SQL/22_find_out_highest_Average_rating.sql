94dd7a9d1258b5565a4707415c865cb5fb3aff39
Venkata Sairam Yanamandra <venkatasairam.y@gmail.com>
2024-07-08 08:11:27 +0530

    movies with highest avg rating

---
+++ SQL/22_find_out_highest_Average_rating.sql
@@ -0,0 +1,114 @@
+
+use tips_db;
+
+CREATE TABLE movies
+(
+    id INT PRIMARY KEY,
+    genre VARCHAR(50),
+    title VARCHAR(100)
+);
+
+-- Create reviews table
+CREATE TABLE reviews
+(
+    movie_id INT,
+    rating DECIMAL(3,1),
+    FOREIGN KEY (movie_id) REFERENCES movies(id)
+);
+
+-- Insert sample data into movies table
+INSERT INTO movies
+    (id, genre, title)
+VALUES
+    (1, 'Action', 'The Dark Knight'),
+    (2, 'Action', 'Avengers: Infinity War'),
+    (3, 'Action', 'Gladiator'),
+    (4, 'Action', 'Die Hard'),
+    (5, 'Action', 'Mad Max: Fury Road'),
+    (6, 'Drama', 'The Shawshank Redemption'),
+    (7, 'Drama', 'Forrest Gump'),
+    (8, 'Drama', 'The Godfather'),
+    (9, 'Drama', 'Schindler''s List'),
+    (10, 'Drama', 'Fight Club'),
+    (11, 'Comedy', 'The Hangover'),
+    (12, 'Comedy', 'Superbad'),
+    (13, 'Comedy', 'Dumb and Dumber'),
+    (14, 'Comedy', 'Bridesmaids'),
+    (15, 'Comedy', 'Anchorman: The Legend of Ron Burgundy');
+
+-- Insert sample data into reviews table
+INSERT INTO reviews
+    (movie_id, rating)
+VALUES
+    (1, 4.5),
+    (1, 4.0),
+    (1, 5.0),
+    (2, 4.2),
+    (2, 4.8),
+    (2, 3.9),
+    (3, 4.6),
+    (3, 3.8),
+    (3, 4.3),
+    (4, 4.1),
+    (4, 3.7),
+    (4, 4.4),
+    (5, 3.9),
+    (5, 4.5),
+    (5, 4.2),
+    (6, 4.8),
+    (6, 4.7),
+    (6, 4.9),
+    (7, 4.6),
+    (7, 4.9),
+    (7, 4.3),
+    (8, 4.9),
+    (8, 5.0),
+    (8, 4.8),
+    (9, 4.7),
+    (9, 4.9),
+    (9, 4.5),
+    (10, 4.6),
+    (10, 4.3),
+    (10, 4.7),
+    (11, 3.9),
+    (11, 4.0),
+    (11, 3.5),
+    (12, 3.7),
+    (12, 3.8),
+    (12, 4.2),
+    (13, 3.2),
+    (13, 3.5),
+    (13, 3.8),
+    (14, 3.8),
+    (14, 4.0),
+    (14, 4.2),
+    (15, 3.9),
+    (15, 4.0),
+    (15, 4.1);
+
+
+SELECT *
+from movies;
+SELECT *
+from reviews;
+
+with
+    cte
+    AS
+    (
+        SELECT
+            m.genre as genre
+            , m.title as title
+            , round(AVG(r.rating), 0) as avg_Rating
+            ,row_number() OVER(partition by genre order by AVG(r.rating) desc) as rnmk
+        from movies m INNER JOIN reviews r ON m.id = r.movie_id
+        GROUP BY  m.genre, m.title
+    )
+SELECT
+    genre
+    , title
+    , REPLICATE('*', avg_Rating) as rating
+from  cte
+WHERE rnmk = 1
+;
+
