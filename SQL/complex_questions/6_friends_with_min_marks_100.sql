15ab0cc38759c508b08c018ba83a51d783b92fde
Venkata Sairam Yanamandra <venkatasairam.y@gmail.com>
2024-07-10 08:26:35 +0530

    p6_friends_with_score > 100

---
+++ SQL/complex_questions/6_friends_with_min_marks_100.sql
@@ -0,0 +1,99 @@
+use tips_db;
+
+drop table friend
+Create table friend
+(
+    pid int,
+    fid int
+)
+insert into friend
+    (pid , fid )
+values
+    ('1', '2');
+insert into friend
+    (pid , fid )
+values
+    ('1', '3');
+insert into friend
+    (pid , fid )
+values
+    ('2', '1');
+insert into friend
+    (pid , fid )
+values
+    ('2', '3');
+insert into friend
+    (pid , fid )
+values
+    ('3', '5');
+insert into friend
+    (pid , fid )
+values
+    ('4', '2');
+insert into friend
+    (pid , fid )
+values
+    ('4', '3');
+insert into friend
+    (pid , fid )
+values
+    ('4', '5');
+drop table person
+create table person
+(
+    PersonID int,
+    Name varchar(50),
+    Score int
+)
+insert into person
+    (PersonID,Name ,Score)
+values('1', 'Alice', '88')
+insert into person
+    (PersonID,Name ,Score)
+values('2', 'Bob', '11')
+insert into person
+    (PersonID,Name ,Score)
+values('3', 'Devis', '27')
+insert into person
+    (PersonID,Name ,Score)
+values('4', 'Tara', '45')
+insert into person
+    (PersonID,Name ,Score)
+values('5', 'John', '63')
+select *
+from person
+select *
+from friend
+;
+
+
+----------------------------------------------------------------
+-- Joining 3 tables
+----------------------------------------------------------------
+
+with
+    cte
+    AS
+    (
+        SELECT
+            person.PersonID
+            , person.Name
+            , person.Score
+            , p2.Score as friends_Score
+            , SUM(p2.Score) OVER(partition BY person.PersonID) as friends_total_score
+        FROM person inner JOIN friend
+            on person.PersonID = friend.pid
+            INNER JOIN person p2
+            on p2.PersonID = friend.fid
+    )
+
+    SELECT 
+    cte.PersonID
+    , cte.Name
+    , COUNT(*) as total_friends
+    , SUM(friends_Score) as friends_total_score
+    from cte 
+    WHERE friends_total_score > 100
+    GROUP BY cte.PersonID, cte.Name
+
+;
