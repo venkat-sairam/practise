0394a11c0cc8f5a78d41832abaf371c7bb4c9712
Venkata Sairam Yanamandra <45553825+venkat-sairam@users.noreply.github.com>
2024-07-14 07:06:10 +0530

    Updated solution 2

--- SQL/complex_questions/14_Customer_retention.sql
+++ SQL/complex_questions/14_Customer_retention.sql
@@ -2,6 +2,7 @@ with cte as
 (
   SELECT *
   , LAG(ordered_date, 1) OVER(PARTITION BY order_id ORDER BY order_date) as last_month_date
+  FROM transactions
 )
 SELECT 
 MONTH(order_date) as month_name
@@ -10,3 +11,12 @@ MONTH(order_date) as month_name
   ) AS Retention_count
 from cte 
 GROUP BY MONTH(order_date)
+
+
+-- SOLUTION-2
+SELECT 
+MONTH(curr_month) AS MONTH_NAME
+  , COUNT(DISTINCT curr_month.id) as retention_count
+FROM transactions as curr_month LEFT JOIN transactions as last_month
+ON curr_month.id = last_month.id AND DATEDIFF(MONTH, last_month.order_date, curr_month.order_date) = 1
+GROUP BY MONTH(curr_month) 
