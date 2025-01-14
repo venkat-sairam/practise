777eaa0236029fa9396fbe2b20d29d79ca739e50
Venkata Sairam Yanamandra <45553825+venkat-sairam@users.noreply.github.com>
2025-01-14 13:24:10 -0700

    added Question-4 solution

--- SQL/week-8/solutions.sql
+++ SQL/week-8/solutions.sql
@@ -46,3 +46,56 @@ LEFT JOIN grouped_Sales_amount as cat
 ON categories.category_id = cat.category_id
 order by total_sales
 ;
+
+-- https://www.namastesql.com/coding-problem/71-department-average-salary
+-- You are provided with two tables: Employees and Departments. 
+-- The Employees table contains information about employees, including their IDs, names, salaries, and department IDs.
+--The Departments table contains information about departments, including their IDs and names.
+-- Your task is to write a SQL query to find the average salary of employees in each department, but only include departments that have more than 2 employees . 
+-- Display department name and average salary round to 2 decimal places. Sort the result by average salary in descending order.
+
+ 
+
+Tables: Employees
++---------------+-------------+
+| COLUMN_NAME   | DATA_TYPE   |
++---------------+-------------+
+| employee_id   | int         |
+| employee_name | varchar(20) |
+| salary        | int         |
+| department_id | int         |
++---------------+-------------+
+Tables: Departments
++-----------------+-------------+
+| COLUMN_NAME     | DATA_TYPE   |
++-----------------+-------------+
+| department_id   | int         |
+| department_name | varchar(10) |
++-----------------+-------------+
+
+with grouped_emp_avg_salary_cte as
+(
+	select 
+    department_id
+    , ROUND(AVG(salary), 2) as average_salary
+    , COUNT(1) AS emp_count
+    from employees
+    GROUP BY department_id
+)
+, filtered_emp_cte AS (
+SELECT 
+ *
+FROM grouped_emp_avg_salary_cte
+WHERE emp_count > 2
+)
+
+SELECT 
+l.department_name
+, r.average_salary
+FROM
+departments l inner join
+filtered_emp_cte r
+on l.department_id = r.department_id
+order by  r.average_salary desc
+;
+
