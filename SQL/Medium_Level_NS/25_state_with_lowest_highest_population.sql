d98ae5377c48c975252a11ea8de1f9431563fd45
Venkata Sairam Yanamandra <45553825+venkat-sairam@users.noreply.github.com>
2025-03-16 16:37:06 -0600

    25_state_with_lowest_highest_population

---
+++ SQL/Medium_Level_NS/25_state_with_lowest_highest_population.sql
@@ -0,0 +1,39 @@
+
+-- for every state, find out the highest and lowest populated cities
+
+CREATE TABLE city_population (
+    state VARCHAR(50),
+    city VARCHAR(50),
+    population INT
+);
+
+-- Insert the data
+INSERT INTO city_population (state, city, population) VALUES ('haryana', 'ambala', 100);
+INSERT INTO city_population (state, city, population) VALUES ('haryana', 'panipat', 200);
+INSERT INTO city_population (state, city, population) VALUES ('haryana', 'gurgaon', 300);
+INSERT INTO city_population (state, city, population) VALUES ('punjab', 'amritsar', 150);
+INSERT INTO city_population (state, city, population) VALUES ('punjab', 'ludhiana', 400);
+INSERT INTO city_population (state, city, population) VALUES ('punjab', 'jalandhar', 250);
+INSERT INTO city_population (state, city, population) VALUES ('maharashtra', 'mumbai', 1000);
+INSERT INTO city_population (state, city, population) VALUES ('maharashtra', 'pune', 600);
+INSERT INTO city_population (state, city, population) VALUES ('maharashtra', 'nagpur', 300);
+INSERT INTO city_population (state, city, population) VALUES ('karnataka', 'bangalore', 900);
+INSERT INTO city_population (state, city, population) VALUES ('karnataka', 'mysore', 400);
+INSERT INTO city_population (state, city, population) VALUES ('karnataka', 'mangalore', 200);
+
+with cte as
+(
+  
+  select *
+  , rank() over(partition by state order by population) as l_rnk
+  , rank() over(partition by state order by population desc) as h_rnk
+  from city_population
+)
+
+select 
+state
+, min(case when l_rnk = 1 then city end ) as l_pop
+, min(case when h_rnk = 1 then city end ) as l_pop
+from cte
+group by state
+  ;
